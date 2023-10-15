e7852d5c33d69cd1c2da45544849a8f2b72565d0
adeliaaaa <monicadelia88@gmail.com>
2023-10-15 17:34:58 +0700

    feat: add canvas with 3d file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Smart Presentation</title>
+        <!-- <link rel="icon" type="image/x-icon" href="assets/SR_logo_03_red.png"> -->
+        <meta charset="UTF-8">
+        <!-- <script type="importmap">
+            {
+                "imports": {
+                "three": "https://unpkg.com/three@0.153.0/build/three.module.js"
+                }
+            } 
+        </script> -->
+        <script src="https://cdn.jsdelivr.net/npm/gsap@3.2.4/dist/gsap.js"></script>
+        <link rel="stylesheet" href="./style/style.css" >
+        <script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
+
+        <script type="importmap">
+            {
+                "imports": {
+                "three": "https://unpkg.com/three@0.154.0/build/three.module.js",
+                "three/addons/": "https://unpkg.com/three@0.154.0/examples/jsm/"
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <div class="home-page">
+            <canvas id="myCanvas">    </canvas>
+
+
+            <div class="loadingScreenContainer" style="display: none">
+                <label for="loadingBar" id='loadingBarLabel'> Loading... </label>
+                <progress id='loadingBar' max='100' value='0'></progress>
+            </div>
+
+            <script type="module" src="script.js"> </script>
+        </div>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,165 @@
+const myCanvas = document.querySelector("#myCanvas");
+// var myText = document.getElementById("myText").textContent;
+
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import {
+	CSS2DRenderer,
+	CSS2DObject,
+} from "three/addons/renderers/CSS2DRenderer.js";
+
+// ----------------------------------- SCENE BACKGROUND COLOR -----------------------------------
+export const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdbe9e9);
+
+// ------------------------------------------- CAMERA -------------------------------------------
+export const camera = new THREE.PerspectiveCamera(
+	45,
+	myCanvas.offsetWidth / myCanvas.offsetHeight
+);
+camera.position.set(6, 4, -4);
+camera.layers.enableAll();
+
+// ----------------------------------------- GRID HELPER ----------------------------------------
+const size = 50;
+const divisions = 50;
+const colorCenterLine = 0xffffff;
+const colorGrid = 0xffffff;
+
+const grid = new THREE.GridHelper(size, divisions, colorCenterLine, colorGrid);
+grid.position.y = -1;
+grid.name = "grid";
+scene.add(grid);
+
+// ------------------------------------------ LIGHTNING -----------------------------------------
+/*
+	Light in 3D scene
+	set(x,y,z)
+		+x front
+		+y up
+		+z left
+*/
+
+// ---------------------------------- LIGHTNING CUSTOM: AMBIENT ---------------------------------
+const ambientLight = new THREE.HemisphereLight(
+	"white", // bright sky color
+	"grey", // dim ground color
+	0 // intensity
+);
+ambientLight.name = "ambientLight";
+scene.add(ambientLight);
+
+// -------------------------------- LIGHTNING CUSTOM: DIRECTIONAL -------------------------------
+var dirLight = new THREE.DirectionalLight(0x404040, 0);
+dirLight.name = "dirLight";
+dirLight.position.set(100, 100, -10);
+dirLight.castShadow = true;
+scene.add(dirLight);
+
+// --------------------------- LIGHTNING DEFAULT: FRONT ABOVE CENTER ----------------------------
+const r = 20;
+const light1 = new THREE.PointLight(0xffffff, 1, 0);
+light1.name = "light1";
+light1.position.set(r, r, 0);
+light1.shadowMapVisible = true;
+scene.add(light1);
+
+// ----------------------------- LIGHTNING DEFAULT: BACK ABOVE LEFT -----------------------------
+const light2 = new THREE.PointLight(0xffffff, 1, 0);
+light2.name = "light2";
+light2.position.set(-0.5 * r, r, 0.866 * r);
+scene.add(light2);
+
+// ---------------------------- LIGHTNING DEFAULT: BACK ABOVE RIGHT -----------------------------
+const light3 = new THREE.PointLight(0xffffff, 1, 0);
+light3.name = "light3";
+light3.position.set(-0.5 * r, r, -0.866 * r);
+scene.add(light3);
+
+// --------------------------- LIGHTNING DEFAULT: CENTER BELOW CENTER ---------------------------
+const light4 = new THREE.PointLight(0xffffff, 1, 0);
+light4.name = "light4";
+light4.position.set(0, -r, 0);
+scene.add(light4);
+
+// --------------------------- LIGHTNING DEFAULT: CENTER BELOW CENTER ---------------------------
+const renderer = new THREE.WebGLRenderer({ canvas: myCanvas });
+renderer.setClearColor(0xff0000, 1.0);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(myCanvas.offsetWidth, myCanvas.offsetHeight);
+
+// --------------------------------------- ORBIT CONTROLS ---------------------------------------
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = "absolute";
+labelRenderer.domElement.style.zIndex = 1;
+labelRenderer.domElement.style.top = "0px";
+document.body.appendChild(labelRenderer.domElement);
+
+export const orbitControls = new OrbitControls(
+	camera,
+	labelRenderer.domElement
+);
+
+// --------------------------------------- 3D FILE LOADER ---------------------------------------
+const loadingScreenBar = document.getElementById("loadingBar");
+const loadingScreenContainer = document.querySelector(
+	".loadingScreenContainer"
+);
+const loadingManager = new THREE.LoadingManager();
+
+loadingManager.onStart = function () {
+	loadingScreenContainer.style.display = "flex";
+};
+
+loadingManager.onProgress = function (url, loaded, total) {
+	loadingScreenBar.value = (loaded / total) * 100;
+};
+
+loadingManager.onLoad = function () {
+	loadingScreenBar.value = 0;
+	loadingScreenContainer.style.display = "none";
+};
+
+export const loader = new GLTFLoader(loadingManager);
+loader.name = "loader";
+
+let path = "files/" + "MSD700_ブレードモデル_MCLA15A.glb";
+
+loader.load(
+	path,
+	function (gltf) {
+		let file3D = gltf.scene;
+		file3D.name = "file3D";
+		scene.add(file3D);
+		file3D.layers.enableAll();
+
+		file3D.position.set(0, -0.95, 0);
+	},
+	undefined,
+	function (error) {
+		console.error(error);
+	}
+);
+
+// ----------------------------------------- RENDER LOOP ----------------------------------------
+renderer.setAnimationLoop(() => {
+	orbitControls.update();
+	labelRenderer.render(scene, camera);
+	renderer.render(scene, camera);
+});
+
+// ---------------------------------------- RESIZE CANVAS ---------------------------------------
+myCanvas.style.width = window.innerWidth + "px";
+myCanvas.style.height = window.innerHeight + "px";
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+
+window.addEventListener("resize", () => {
+	myCanvas.style.width = window.innerWidth + "px";
+	myCanvas.style.height = window.innerHeight + "px";
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	labelRenderer.setSize(window.innerWidth - 0.5, window.innerHeight - 0.5);
+});
